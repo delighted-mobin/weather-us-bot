1f6248855058059c29c0f74d8823c5876764afef
Dm <mobin.mm.u@gmail.com>
2026-02-23 17:14:30 +0330

    sfrt

--- site.py
+++ site.py
@@ -214,8 +214,7 @@ def create_map(state: str = "mazandaran", model: str = "gfs", type: list = ["rai
         headers[0]["Referer"] = i["link"]
         send_message(id, "16")
         req = session.get(i["link"],headers=headers[0])
-        print(str(req.content))
-        send_message(id, str(req.content))
+        send_message(id, str(req.content)[0,10])
         try:
             with open(str(i["run"])+".png", "wb") as f:
                 f.write(req.content)
